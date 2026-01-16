263d71d7f7c78244caafd0194d846a18accdb13f
Egor Gruzdev <egogryzdev@team.amocrm.com>
2026-01-16 15:50:59 +0300

    feat: add refresh

--- src/modules/auth/auth.schemas.ts
+++ src/modules/auth/auth.schemas.ts
@@ -2,14 +2,14 @@ import { z } from "zod";
 
 export const registerSchema = z.object({
   email: z.string(),
-  password: z.string().min(8),
+  password: z.string().min(8, "Password must be at least 8 characters"),
   name: z.string().optional(),
 });
 
 export type RegisterDto = z.infer<typeof registerSchema>;
 
 export const loginSchema = z.object({
-  email: z.string().email(),
+  email: z.string(),
   password: z.string().min(1),
 });
 
